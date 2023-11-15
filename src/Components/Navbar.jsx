c207628bb12787a056a977af5a8bb346e0b38935
Hari Prasad <101623030+Hari6360@users.noreply.github.com>
2023-11-15 18:01:04 +0530

    setup:create-react-app-template with Tailwind css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Navbar = () => {
-	return <div></div>;
+	return (
+		<div>
+			<div className="flex justify-between bg-gray-500 p-4 text-white">
+				<div>Logo</div>
+				<div className="flex items-center space-x-8 ">
+					<input
+						type="search"
+						name="search"
+						className="rounded-md p-2 focus:outline-none border border-gray-400"
+					/>
+					<ul className="flex items-center space-x-4">
+						<li>
+							<a href="" className="text-white ">
+								Signup
+							</a>
+						</li>
+						<span>|</span>
+						<li>
+							<a href="" className="text-white ">
+								Login
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
